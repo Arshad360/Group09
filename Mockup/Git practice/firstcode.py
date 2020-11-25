d515e8c6afb259aa1b7c6de234a9612580f3f9ce
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2020-11-26 03:59:02 +0600

    Learning #4: practice code added

--- Mockup/Git practice/firstcode.py
+++ Mockup/Git practice/firstcode.py
@@ -1,1 +1,15 @@
 print('This is a test code')
+
+a = 5
+b = 4
+c = 2
+
+# Calculate the semi-perimeter
+
+s = (a+b+c)/2
+
+# Area Calculation
+
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+
+print('The area of the triangle is %0.2f' %area)
