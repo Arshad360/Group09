60828a533e41d0f5ca6561495192af9c9e75ef11
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2020-12-03 01:15:52 +0600

    Documentation #14 : Path created for templates and static
    Path ceated for 'Templates' and 'Static' folder inside the project in settings.py

--- Code/Easy_Shopify/Easy_Shopify/settings.py
+++ Code/Easy_Shopify/Easy_Shopify/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR,'Templates')
+STATIC_DIR=os.path.join(BASE_DIR,'Static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'Easy_Shopify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
