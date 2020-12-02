343a2a467e7fbc19bbe17d18d9caa476606bfcbc
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2020-12-03 00:57:36 +0600

    Documentation #14 : Application installed in the settings.py
    Easy_Shopify_app installed in the installed app section in settings.py file.

--- Code/Easy_Shopify/Easy_Shopify/settings.py
+++ Code/Easy_Shopify/Easy_Shopify/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Easy_Shopify_app',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
