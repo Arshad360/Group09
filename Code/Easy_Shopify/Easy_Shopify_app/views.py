afc0618b423278c0afe1e2a4f2586af1b70bdb00
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2020-12-06 03:25:44 +0600

    Enhancement #16 : Landing page created
    Landing page of the application created.

--- Code/Easy_Shopify/Easy_Shopify_app/views.py
+++ Code/Easy_Shopify/Easy_Shopify_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect,HttpResponse
 
 # Create your views here.
+
+def home_view(request):
+
+    return render(request,'Easy_Shopify_app/index.html')
