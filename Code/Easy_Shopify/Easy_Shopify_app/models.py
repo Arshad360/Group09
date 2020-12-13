efe6ad4de13f72165c28b9a088ea3399c87d7fb4
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2020-12-13 23:29:01 +0600

    Enhancement #21 : Form and model created for  customer signup
    For customer signup now the file will be stored in database.

--- Code/Easy_Shopify/Easy_Shopify_app/models.py
+++ Code/Easy_Shopify/Easy_Shopify_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Customer(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=11,null=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return self.user.first_name
--- Code/Easy_Shopify/Easy_Shopify_app/views.py
+++ Code/Easy_Shopify/Easy_Shopify_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponseRedirect,HttpResponse
+from . import forms,models
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import Group, User, auth
+from django.conf import settings
 
-# Create your views here.
 
 def home_view(request):
 
@@ -15,6 +17,23 @@ def customerclick_view(request):
     
     return render(request, 'Easy_Shopify_app/customerlogin.html')
 
-def customersignupclick_view(request):                                            
-                   
-    return render(request, 'Easy_Shopify_app/customersignup.html')
+
+def customer_signup_view(request):
+    userForm=forms.CustomerUserForm()
+    customerForm=forms.CustomerForm()     
+    easy_shopify_app={'userForm':userForm,'customerForm':customerForm}
+    if request.method=='POST':
+        userForm=forms.CustomerUserForm(request.POST)
+        customerForm=forms.CustomerForm(request.POST,request.FILES)
+        if userForm.is_valid() and customerForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            customer=customerForm.save(commit=False)
+            customer.user=user
+            customer.save()
+            my_customer_group = Group.objects.get_or_create(name='CUSTOMER')
+            my_customer_group[0].user_set.add(user)
+        return HttpResponseRedirect('customerlogin')
+    
+    return render(request,'Easy_Shopify_app/customersignup.html',context=easy_shopify_app)
