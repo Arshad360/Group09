5fcd914ce80f66f5a45e2127f9e47057a1aa0be5
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2021-01-15 02:31:05 +0600

    Enhancement #47 : urls path added for the remove from cart function
    In urls.py file, new path added for the view function remov_from_cart.

--- Code/Easy_Shopify/Easy_Shopify/urls.py
+++ Code/Easy_Shopify/Easy_Shopify/urls.py
@@ -37,4 +37,5 @@ urlpatterns = [
     path('admin-products', views.admin_products_view,name='admin-products'),
     path('admin-add-product', views.admin_add_product_view,name='admin-add-product'),
     path('add-to-cart/<int:pk>', views.add_to_cart_view,name='add-to-cart'),
+    path('remove-from-cart/<int:pk>', views.remove_from_cart_view,name='remove-from-cart'),
 ]
