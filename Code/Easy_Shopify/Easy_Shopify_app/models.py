bdad7d3a5e2e04fa2ab9a641c1a6e62fd8e5097a
Mohammad Arshadul Alam Rakib <68860229+Arshad360@users.noreply.github.com>
2021-01-16 17:40:12 +0600

    Enhancement #50 : feedback models created
    In models.py file, new feedback model class created where the variables are declared.

--- Code/Easy_Shopify/Easy_Shopify_app/models.py
+++ Code/Easy_Shopify/Easy_Shopify_app/models.py
@@ -22,3 +22,9 @@ class Product(models.Model):
     def __str__(self):
         return self.name
   
+class Feedback(models.Model):
+    name=models.CharField(max_length=40)
+    feedback=models.CharField(max_length=500)
+    date= models.DateField(auto_now_add=True,null=True)
+    def __str__(self):
+        return self.name
